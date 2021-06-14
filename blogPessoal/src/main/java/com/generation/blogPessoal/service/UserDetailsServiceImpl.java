a9ed4913498b62ec5e4fa185f51d9cdbe922c7b7
jovieira127 <jonatasmichel9@gmail.com>
2021-06-14 15:15:02 -0300

    Swagger

--- blogPessoal/src/main/java/com/generation/blogPessoal/service/UserDetailsServiceImpl.java
+++ blogPessoal/src/main/java/com/generation/blogPessoal/service/UserDetailsServiceImpl.java
@@ -21,7 +21,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		Optional<Usuario> user = usuarioRepository.findByUsuario(username);
-		user.orElseThrow(() -> new UsernameNotFoundException(username + "not found."));
+		user.orElseThrow(() -> new UsernameNotFoundException(username + "Não encontrado"));
 		return user.map(UserDetailsImpl::new).get();
 	}
 
